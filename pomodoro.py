e394fcd7613738bf146d7fd2e10e423a09faa269
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-10-03 13:02:08 +0300

    labels and buttons added

--- pomodoro.py
+++ pomodoro.py
@@ -12,6 +12,17 @@ class App:
         self.root.geometry("+650+300")
         self.root.config(bg='pink')
 
+        # Labels
+        self.label = tk.Label(self.root, text='Pomodoro Timer', padx=30, pady=20, font=('Georgia', 18), bg='crimson')
+        self.label.pack()
+
+        # Buttons
+        tk.Button(self.root, text='Pomodoro', bg='pink', activebackground='lightgrey', font=('Georgia', 16)).place(x=40, y=100)
+        tk.Button(self.root, text='Long Break', bg='pink', activebackground='lightgrey', font=('Georgia', 16)).place(x=230, y=100)
+        tk.Button(self.root, text='Short Break', bg='pink', activebackground='lightgrey', font=('Georgia', 16)).place(x=430, y=100)
+        tk.Button(self.root, text='START', foreground='crimson', padx=50, pady=0, font=('Georgia', 22)).place(x=200, y=300)
+
+
         self.root.mainloop()
 
 
