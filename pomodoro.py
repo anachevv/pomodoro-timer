f1c582c79d6336f0d2ed1d56e6601c836c423776
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-10-03 12:48:07 +0300

    main app method added

--- pomodoro.py
+++ pomodoro.py
@@ -7,6 +7,11 @@ class App:
     def __init__(self):
         self.root = tk.Tk()
 
+        self.root.title("Pomodoro Timer")
+        self.root.minsize(600, 400)
+        self.root.geometry("+650+300")
+        self.root.config(bg='pink')
+
         self.root.mainloop()
 
 
