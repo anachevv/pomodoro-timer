3b1e82074d3122a4f75a2aefa61a63e2410b519d
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2022-10-03 13:47:58 +0300

    clock label and clock function added

--- pomodoro.py
+++ pomodoro.py
@@ -2,28 +2,36 @@ import tkinter as tk
 from time import strftime
 
 
-class App:
+class App(tk.Tk):
 
     def __init__(self):
-        self.root = tk.Tk()
+        super().__init__()
 
-        self.root.title("Pomodoro Timer")
-        self.root.minsize(600, 400)
-        self.root.geometry("+650+300")
-        self.root.config(bg='pink')
+        self.title("Pomodoro Timer")
+        self.minsize(600, 400)
+        self.geometry("+650+300")
+        self.config(bg='pink')
 
         # Labels
-        self.label = tk.Label(self.root, text='Pomodoro Timer', padx=30, pady=20, font=('Georgia', 18), bg='crimson')
+        self.label = tk.Label(self, text='Pomodoro Timer', padx=30, pady=20, font=('Georgia', 18), bg='crimson')
         self.label.pack()
 
+        self.clock_label = tk.Label(text='', background='orangered', foreground='white', font=('Roboto', 25))
+        self.clock_label.pack(anchor='center', padx=200, pady=100)
+        self.display_time()
+
         # Buttons
-        tk.Button(self.root, text='Pomodoro', bg='pink', activebackground='lightgrey', font=('Georgia', 16)).place(x=40, y=100)
-        tk.Button(self.root, text='Long Break', bg='pink', activebackground='lightgrey', font=('Georgia', 16)).place(x=230, y=100)
-        tk.Button(self.root, text='Short Break', bg='pink', activebackground='lightgrey', font=('Georgia', 16)).place(x=430, y=100)
-        tk.Button(self.root, text='START', foreground='crimson', padx=50, pady=0, font=('Georgia', 22)).place(x=200, y=300)
+        tk.Button(self, text='Pomodoro', bg='pink', activebackground='lightgrey', font=('Georgia', 16)).place(x=40, y=100)
+        tk.Button(self, text='Long Break', bg='pink', activebackground='lightgrey', font=('Georgia', 16)).place(x=230, y=100)
+        tk.Button(self, text='Short Break', bg='pink', activebackground='lightgrey', font=('Georgia', 16)).place(x=430, y=100)
+        tk.Button(self, text='START', foreground='crimson', padx=50, pady=0, font=('Georgia', 22)).place(x=200, y=300)
+
+    def display_time(self):
+        string = strftime('%H:%M:%S %p')
+        self.clock_label.config(text=string)
+        self.clock_label.after(1000, self.display_time)
 
 
-        self.root.mainloop()
-
-
-App()
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
